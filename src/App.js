39b5105e4163e34216cc182c14d16c644d5363b4
govind <govindwebdev@gmail.com>
2024-10-24 11:18:09 +0530

    Exists todolist

--- src/App.js
+++ src/App.js
@@ -1,8 +1,22 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+  let [todolist,setTodolist]=useState([])
+
   let saveToDoList=(event) => {
+
+    let toName = event.target.toName.value;
+
+    if(!todolist.includes(toName)){
+      let finalToDolist=[...todolist,toName]
+      setTodolist(finalToDolist)
+
+    }else {
+      alert("ToDo Name Allready Exists...")
+    }
+
     event.preventDefault();
   }
 
@@ -10,7 +24,7 @@ function App() {
     <div className="App">
       <h1>ToDo List</h1>
       <form onSubmit={saveToDoList}>
-        <input type="text" /> <button>Save</button>
+        <input type="text" name="toName"/> <button>Save</button>
       </form>
 
     </div>
